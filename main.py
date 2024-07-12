98991d74b0363defe6d599f645041bb2f35f211c
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2024-07-11 22:35:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import tkinter as tk
-from gui import Aplicacion
+from gui import SistemaDeAyuda
+from login import LoginRegistro
 
 def main():
     root = tk.Tk()
-    app = Aplicacion(root)
+    app = LoginRegistro(root, launch_main_app)
+    root.mainloop()
+
+def launch_main_app(tipo_usuario):
+    root = tk.Tk()
+    app = SistemaDeAyuda(root, tipo_usuario)
     root.mainloop()
 
 if __name__ == "__main__":
